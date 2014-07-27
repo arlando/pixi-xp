42ad41ed0117ef3c7917d2c0016276607ebe9977
Arlando Battle <arlando@1stdibs.com>
2014-07-27 15:00:20 -0400

    better live reload

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,11 +3,28 @@ var gulp = require('gulp');
 var express = require('express');
 var livereload = require('gulp-livereload');
 var browserify = require('gulp-browserify');
+var embedlr = require('gulp-embedlr');
 var lrserver;
+var debug = process.env.NODE_ENV || 'development';
+
 
 var EXPRESS_PORT = 4000;
 var EXPRESS_ROOT = __dirname;
 
+/**
+ * Embeds live reload snippets on html files in static
+ */
+function embedLrSnippet() {
+    if (debug === 'development') {
+        gulp.src('./static/*.html')
+            .pipe(embedlr())
+            .pipe(gulp.dest('./static'));
+    }
+}
+
+/**
+ * Start the Express server.
+ */
 function startExpress() {
     var app = express();
     app.use(express.static(EXPRESS_ROOT + '/static'));
@@ -16,6 +33,7 @@ function startExpress() {
 }
 
 gulp.task('default', ['scripts'], function() {
+    embedLrSnippet();
     startExpress();
     lrserver = livereload();
     gulp.watch('js/**', ['scripts']);
@@ -28,7 +46,7 @@ gulp.task('scripts', function() {
     gulp.src('./js/main.js')
         .pipe(browserify({
             insertGlobals: true,
-            debug: !process.env.NODE_ENV
+            debug: (debug === 'development')
         }))
         .pipe(gulp.dest('./static/build/'));
 });
